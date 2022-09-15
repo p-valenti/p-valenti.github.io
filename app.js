f2e58398e7c6a88a21911c48e2dacb95d14a3188
Valentina Popova <solval89@gmail.com>
2022-09-16 00:57:19 +0300

    fixed slidebar. Added text typing

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ menuBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
     sidebar.classList.remove("show-sidebar");
 });
+
+// let i=0;
+// const txt = 'Hi there! My name is Valentina';
+// const speed = 50;
+//
+// function typingText() {
+//     if (i < txt.length) {
+//         document.getElementById("hello").innerHTML += txt.charAt(i);
+//         i++;
+//         setTimeout(typingText, speed);
+//     }
+// }
